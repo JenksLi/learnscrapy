ea9a580b14e0e3692f9c99449449278617495fd8
JnkinsLee <happylearnlife@foxmail.com>
2017-04-06 09:39:49 +0800

    scrapy练习

--- scrapy3/pipelines.py
+++ scrapy3/pipelines.py
@@ -7,11 +7,13 @@
 
 import json
 from scrapy.pipelines.images import ImagesPipeline
+from scrapy.exporters import JsonItemExporter   #scrapy自带item导出为json方法
 
 class Scrapy3Pipeline(object):
     def process_item(self, item, spider):
         return item
 
+#自定义json导出
 class JsonWithEncodingPipeline(object):
     def __init__(self):
         self.file = open('article.json','w',encoding='utf8')
@@ -21,6 +23,19 @@ class JsonWithEncodingPipeline(object):
         return item
     def spider_closed(self, spider):
         self.file.close()
+
+#调用scrapy提供的json export导出json文件
+class JsonExporterPipeline(object):
+    def __init__(self):
+        self.file = open('ArticleJosnExporter.json','wb')
+        self.exporter = JsonItemExporter(self.file,encoding='utf8',ensure_ascii=False)
+        self.exporter.start_exporting()
+    def spider_closed(self, spider):
+        self.exporter.finish_exporting()
+        self.file.close()
+    def process_item(self, item, spider):
+        self.exporter.export_item(item)
+        return item
 
 class ScrapyImagePipeline(ImagesPipeline):
     def item_completed(self, results, item, info):
